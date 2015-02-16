12d7fab596a9a0c02b3c89c5be5f953cfea05802
lam9009 <lam9009@yahoo.com>
2015-02-16 13:07:57 -0800

    Initial Commit

---
+++ PassingData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PassingData
+//
+//  Created by Alfred on 16/2/15.
+//  Copyright (c) 2015 Maptier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PassingData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PassingData
+//
+//  Created by Alfred on 16/2/15.
+//  Copyright (c) 2015 Maptier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
